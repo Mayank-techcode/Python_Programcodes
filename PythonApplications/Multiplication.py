a003e6f63d14b6124959d3d455f776fafa215f5b
Mayank Salunkhe <MayankSalunkhe.work@gmail.com>
2026-09-03 21:32:41 +0530

    Add files via upload

---
+++ PythonApplications/Multiplication.py
@@ -0,0 +1,13 @@
+def Multiplication(No1, No2):
+    Ans = No1 * No2
+    return Ans
+
+print("Enter first Number: ")
+Value1 = int(input())
+
+print("Enter Second Number: ")
+Value2 = int(input())
+
+Ret = Multiplication(Value1, Value2)
+
+print("Your result is",Ret)
