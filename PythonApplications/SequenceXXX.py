895aaa5d5a6c3ece592e76ed8ffa7b46f63f9271
Mayank Salunkhe <MayankSalunkhe.work@gmail.com>
2026-09-08 16:00:19 +0530

    Add files via upload

---
+++ PythonApplications/SequenceXXX.py
@@ -0,0 +1,15 @@
+
+def CircleArea(Rad, PI = 3.14):
+    Area = PI * Rad * Rad
+    return Area
+
+def main():
+    print("Enter Radius of Circle: ")
+    radius = float(input())
+
+    result = CircleArea(radius)
+
+    print("Area of circle is : ", result)
+
+if __name__ == "__main__":
+    main()
